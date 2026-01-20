85ebbc73645e74cb143828a24e9e4b6fe6e347df
alessandraciccone <alessandraciccone@icloud.com>
2026-01-20 14:38:31 +0100

    Initial commit

---
+++ src/main/java/acicone/RapportoInterventi/RapportoInterventiApplication.java
@@ -0,0 +1,13 @@
+package acicone.RapportoInterventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapportoInterventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapportoInterventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/acicone/RapportoInterventi/RapportoInterventiApplicationTests.java
@@ -0,0 +1,13 @@
+package acicone.RapportoInterventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapportoInterventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
